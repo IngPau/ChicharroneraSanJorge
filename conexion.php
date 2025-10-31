bbcc876b9eb7822917a22a2d0d8421d453fa0d19
Natareno2124 <kevinnajera24@gmail.com>
2025-10-31 10:26:56 -0600

    Cambios en ventas, ahora afecta al inventario

--- conexion.php
+++ conexion.php
@@ -1,12 +1,9 @@
 <?php
 
 function conectar() {
-
-    $db = new mysqli("localhost","root", "", "CSJ_db",3307);
-
-  
-
-    if (!$db)
+    $db = new mysqli("localhost","root", "123456", "CSJ_db");
+    //$db = new mysqli("localhost","root", "", "CSJ_db",3307); - Conexion KEVIN
+  if (!$db)
         die("no hay conexion a la base de datos");
 
     return $db;
